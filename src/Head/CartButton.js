53d3df782155140865861a145f17b92b51bc2c34
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-09 21:41:36 +0400

    09.08

--- src/Head/CartButton.js
+++ src/Head/CartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import CartContext from "../store/cart-context";
 import classes from "../Head/HeaderCartButton.module.css";
@@ -8,9 +8,26 @@ const CartButton = (props) => {
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount
   }, 0);
+
+  const [btnIsHighlighted, setBtnIsHighlighted]=useState(false);
+const {items}=cartCtx;
+  const btnClasses=`${classes.button} ${btnIsHighlighted ? classes.bump:''}`;
+
+  useEffect(()=>{                  //Cart Bump while pressing add button
+    if(items.length===0){
+      return;
+    }
+    setBtnIsHighlighted (true);
+    const timer=setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    }, 300);
+    return ()=>{
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}>
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}> 
         <CartIcon />
       </span>
       <span>Your Cart</span>
